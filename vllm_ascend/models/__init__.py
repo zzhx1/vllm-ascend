344228a5da6130bf1d7e5f3c3d04cc8b661748ff
Mengqing Cao <cmq0113@163.com>
2025-04-07 10:56:12 +0800

    [deepseek][bugfix] support deepseek quant (#469)
    - support deepseek quant
      - add w8a8_dynamic quant
    see #391
    
    Signed-off-by: MengqingCao <cmq0113@163.com>
    Co-authored-by: zzzzwwjj <1183291235@qq.com>

--- vllm_ascend/models/__init__.py
+++ vllm_ascend/models/__init__.py
@@ -7,3 +7,11 @@ def register_model():
     ModelRegistry.register_model(
         "Qwen2VLForConditionalGeneration",
         "vllm_ascend.models.qwen2_vl:CustomQwen2VLForConditionalGeneration")
+
+    ModelRegistry.register_model(
+        "DeepseekV2ForCausalLM",
+        "vllm_ascend.models.deepseek_v2:CustomDeepseekV2ForCausalLM")
+
+    ModelRegistry.register_model(
+        "DeepseekV3ForCausalLM",
+        "vllm_ascend.models.deepseek_v2:CustomDeepseekV3ForCausalLM")
