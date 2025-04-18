23f85e3f7425d0041abc28a46d6010740ec36bbc
whx <56632993+whx-sjtu@users.noreply.github.com>
2025-04-18 08:49:48 +0800

    [BugFix] Fix scheduler problems in last PR. (#558)
    This PR Fixes scheduler problems in last PR:
    1. change position of DT test to validate it.
    2. fix format of copyright.
    
    Signed-off-by: whx-sjtu <2952154980@qq.com>

--- tests/singlecard/test_scheduler.py
+++ tests/singlecard/test_scheduler.py
@@ -1,5 +1,8 @@
 #
 # Copyright (c) 2025 Huawei Technologies Co., Ltd. All Rights Reserved.
+# This file is a part of the vllm-ascend project.
+# Adapted from vllm-project/vllm/blob/main/tests/models/utils.py
+# Copyright 2023 The vLLM team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +15,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# This file is a part of the vllm-ascend project.
 #
 from typing import Optional
 
