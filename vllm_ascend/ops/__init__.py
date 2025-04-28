1fce70a2fb2602170781773104a69c69beb16161
wangxiyuan <wangxiyuan1007@gmail.com>
2025-04-28 21:57:01 +0800

    [Model] Support common fused moe ops for moe model, such as Qwen3Moe (#709)
    vllm-ascend now only support moe for deepseek. We should add common moe
    support back
    
    Signed-off-by: wangxiyuan <wangxiyuan1007@gmail.com>

--- vllm_ascend/ops/__init__.py
+++ vllm_ascend/ops/__init__.py
@@ -19,6 +19,7 @@ import torch
 import torch_npu  # noqa: F401
 
 import vllm_ascend.ops.activation  # noqa
+import vllm_ascend.ops.common_fused_moe  # noqa
 import vllm_ascend.ops.fused_moe  # noqa
 import vllm_ascend.ops.layernorm  # noqa
 import vllm_ascend.ops.rotary_embedding  # noqa
