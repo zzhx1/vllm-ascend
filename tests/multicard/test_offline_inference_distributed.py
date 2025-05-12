8e4e791fcda18e5fa9d5f85b1ffe58ad45cef691
Li Wang <wangli858794774@gmail.com>
2025-05-12 14:59:17 +0800

    [CI] Add deepseek-v2-lite test (#631)
    ### What this PR does / why we need it?
    Add deepseek-v2-lite test, part of #499 
    ---------
    
    Signed-off-by: wangli <wangli858794774@gmail.com>

--- tests/multicard/test_offline_inference_distributed.py
+++ tests/multicard/test_offline_inference_distributed.py
@@ -28,10 +28,12 @@ import vllm  # noqa: F401
 from tests.conftest import VllmRunner
 
 os.environ["PYTORCH_NPU_ALLOC_CONF"] = "max_split_size_mb:256"
+os.environ["VLLM_USE_MODELSCOPE"] = "True"
 
 
 @pytest.mark.parametrize("model, distributed_executor_backend", [
     ("Qwen/QwQ-32B", "mp"),
+    ("deepseek-ai/DeepSeek-V2-Lite", "mp"),
 ])
 def test_models_distributed(model: str,
                             distributed_executor_backend: str) -> None:
