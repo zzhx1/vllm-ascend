d2f87ed9ccd565d323b9e177856fe59c46b8467b
Shanshan Shen <467638484@qq.com>
2025-06-09 15:05:11 +0800

    [Patch] Remove `spec_decode.metrics` patch (#1016)
    ### What this PR does / why we need it?
    Remove `spec_decode.metrics` patch as this has been resolved in
    https://github.com/vllm-project/vllm/pull/16983 (include in vllm
    `v0.9.0`).
    
    Returns a CUDA event recording when the copy is complete **--after
    modified-->** Returns a device event (NPU Event for vllm-ascend)
    recording when the copy is complete.
    
    Signed-off-by: shen-shanshan <467638484@qq.com>

--- vllm_ascend/patch/worker/patch_common/__init__.py
+++ vllm_ascend/patch/worker/patch_common/__init__.py
@@ -20,7 +20,6 @@
 import vllm_ascend.patch.worker.patch_common.patch_utils  # noqa isort:skip
 import vllm_ascend.patch.worker.patch_common.patch_distributed  # noqa
 import vllm_ascend.patch.worker.patch_common.patch_eagle  # noqa
-import vllm_ascend.patch.worker.patch_common.patch_metrics  # noqa
 import vllm_ascend.patch.worker.patch_common.patch_minicpm  # noqa
 import vllm_ascend.patch.worker.patch_common.patch_multi_step_worker  # noqa
 import vllm_ascend.patch.worker.patch_common.patch_sampler  # noqa
