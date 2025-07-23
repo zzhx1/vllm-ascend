734eb68129ec712693e3bc26653fc3b1c29d2f63
weichen <132029610+Pr0Wh1teGivee@users.noreply.github.com>
2025-07-23 09:19:39 +0800

    [0.9.1][Perf] Use fused ops npu_top_k_top_p (#1920)
    ### What this PR does / why we need it?
    Use fused ops torch_npu.npu_top_k_top_p(logits, p, k) when p and k are
    not None, otherwise fallback to the original one. The replacement will
    take place automatically when VLLM_ASCEND_ENABLE_TOPK_OPTIMIZE=1 .
    
    This patch are using npu_top_k_top_p which required
    torch_npu>=2.5.1.post1.dev20250619
    
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    ut & e2e
    
    
    Signed-off-by: Pr0Wh1teGivee <calvin_zhu0210@outlook.com>

--- vllm_ascend/patch/worker/patch_common/patch_sampler.py
+++ vllm_ascend/patch/worker/patch_common/patch_sampler.py
@@ -19,6 +19,7 @@
 from typing import Optional
 
 import torch
+import torch_npu
 from vllm.v1.sample.ops.topk_topp_sampler import TopKTopPSampler, random_sample
 from vllm.v1.sample.sampler import Sampler
 
@@ -46,11 +47,15 @@ def apply_min_p(
     return logits
 
 
-def _apply_top_k_top_p(
+def apply_top_k_top_p(
     logits: torch.Tensor,
-    p: torch.Tensor,
     k: torch.Tensor,
+    p: torch.Tensor,
 ) -> torch.Tensor:
+    if p is not None and k is not None:
+        # npu_top_k_top_p's parameter order is (logits, p, k), not (logits, k, p)
+        return torch_npu.npu_top_k_top_p(logits, p, k)
+
     probs = logits.softmax(dim=-1)
     probs_sort, _ = probs.sort(dim=-1, descending=False)
 
@@ -91,7 +96,7 @@ def topk_topp_forward_native(
 
     The logits tensor may be updated in-place.
     """
-    logits = _apply_top_k_top_p(logits, k, p)
+    logits = apply_top_k_top_p(logits, k, p)
     probs = logits.softmax(dim=-1, dtype=torch.float32)
     return random_sample(probs, generators)
 
