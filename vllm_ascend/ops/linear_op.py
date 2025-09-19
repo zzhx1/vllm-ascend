475db6f1100b0f7a566c498bc5ede2ddeaccf42e
zzhx1 <zzh_201018@outlook.com>
2025-09-19 11:43:08 +0800

    fix: explicitly setting the tensor shape of otp output to fix shape infer error
    Signed-off-by: zzhx1 <zzh_201018@outlook.com>

--- vllm_ascend/ops/linear_op.py
+++ vllm_ascend/ops/linear_op.py
@@ -291,6 +291,7 @@ class OProjRowParallelOp(CustomRowParallelOp):
 
         # otp-specific: Combine partial results across devices
         output = self.comm_group.reduce_scatter(output_parallel, dim=0)
+        output = output.view(input_.shape[0], self.output_size)
 
         # Handle bias return based on configuration
         output_bias = self.bias if self.skip_bias_add else None
