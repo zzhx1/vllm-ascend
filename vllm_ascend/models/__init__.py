31dda3f557cab4f4a6bcbf2a3d982cb2591b68e9
Peipei <51022443+booker123456@users.noreply.github.com>
2025-09-25 18:50:12 +0800

    [Model]Add support for qwen3_vl and qwen3_vl_moe (#3103)
    ### What this PR does / why we need it?
    This PR is for the adaptation and optimization of qwen3_vl and
    qwen3_vl_moe on the Ascend platform.
    ### Does this PR introduce _any_ user-facing change?
    None
    ### How was this patch tested?
    
    
    - vLLM version: v0.10.2
    - vLLM main:
    https://github.com/vllm-project/vllm/commit/b1068903fdca26cf6b4a1a51a32c3365ce3ac636
    
    ---------
    
    Signed-off-by: booker123456 <945658361@qq.com>

--- vllm_ascend/models/__init__.py
+++ vllm_ascend/models/__init__.py
@@ -8,6 +8,16 @@ def register_model():
         "Qwen2VLForConditionalGeneration",
         "vllm_ascend.models.qwen2_vl:AscendQwen2VLForConditionalGeneration")
 
+    ModelRegistry.register_model(
+        "Qwen3VLMoeForConditionalGeneration",
+        "vllm_ascend.models.qwen2_5_vl_without_padding:AscendQwen3VLMoeForConditionalGeneration"
+    )
+
+    ModelRegistry.register_model(
+        "Qwen3VLForConditionalGeneration",
+        "vllm_ascend.models.qwen2_5_vl_without_padding:AscendQwen3VLForConditionalGeneration"
+    )
+
     if envs_ascend.USE_OPTIMIZED_MODEL:
         ModelRegistry.register_model(
             "Qwen2_5_VLForConditionalGeneration",
