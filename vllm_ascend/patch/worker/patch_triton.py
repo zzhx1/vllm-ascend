78bf211539b0530e3442ca112a00e95e8390ced7
QilaiZhang <46159292+QilaiZhang@users.noreply.github.com>
2025-12-11 15:52:39 +0800

    [OPS] support triton causal_conv1d_fn ops (#4119)
    ### What this PR does / why we need it?
    Support triton causal_conv1d_fn ops.
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    CI passed with new added/existing test.
    
    - vLLM version: v0.12.0
    - vLLM main:
    https://github.com/vllm-project/vllm/commit/ad32e3e19ccf0526cb6744a5fed09a138a5fb2f9
    
    ---------
    
    Signed-off-by: QilaiZhang <245706640@qq.com>

--- vllm_ascend/patch/worker/patch_triton.py
+++ vllm_ascend/patch/worker/patch_triton.py
@@ -4,7 +4,7 @@ from vllm_ascend.ops.triton.fla.chunk import chunk_gated_delta_rule
 from vllm_ascend.ops.triton.fla.layernorm_guard import LayerNormFn
 from vllm_ascend.ops.triton.fla.sigmoid_gating import \
     fused_recurrent_gated_delta_rule_fwd_kernel
-from vllm_ascend.ops.triton.mamba.casual_conv1d import (
+from vllm_ascend.ops.triton.mamba.causal_conv1d import (
     causal_conv1d_fn, causal_conv1d_update_npu)
 
 vllm.model_executor.layers.mamba.ops.causal_conv1d.causal_conv1d_update = causal_conv1d_update_npu
