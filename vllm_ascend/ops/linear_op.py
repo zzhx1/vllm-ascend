1dbc784c782768f1cf3935a372586a44941b8490
zzhx1 <zzh_201018@outlook.com>
2025-12-12 14:25:01 +0800

    add mlp_tp logic  exclude moe layer
    Signed-off-by: zzhx1 <zzh_201018@outlook.com>
    Co-authored-by: 子潜 <ziqian@U-DMKXH32D-2015.local>

--- vllm_ascend/ops/linear_op.py
+++ vllm_ascend/ops/linear_op.py
@@ -36,6 +36,7 @@ How to extend a new linear op? Taking column parallel op as an example:
 Row parallel op follows a similar approach - inherit from RowColumnParallelOp and register the new class in get_row_parallel_op.
 """
 
+import re
 from typing import Optional, Union
 
 import torch
@@ -605,7 +606,8 @@ class SequenceRowParallelOp(CustomRowParallelOp):
 def _get_column_parallel_op(
         prefix, layer
 ) -> Optional[Union[MLPColumnParallelOp, SequenceColumnParallelOp]]:
-    if mlp_tp_enable() and "gate_up_proj" in prefix:
+    if "gate_up_proj" in prefix and mlp_tp_enable(
+    ) and not maybe_is_moe_layer(prefix):
         return MLPColumnParallelOp(layer)
     if enable_sp():
         if "shared_expert" in prefix:
@@ -629,7 +631,8 @@ def _get_row_parallel_op(
 ) -> Optional[Union[MLPRowParallelOp, OProjRowParallelOp,
                     Flashcomm2OProjRowParallelOp, MatmulAllreduceRowParallelOp,
                     SequenceRowParallelOp]]:
-    if "down_proj" in prefix and mlp_tp_enable():
+    if "down_proj" in prefix and mlp_tp_enable(
+    ) and not maybe_is_moe_layer(prefix):
         return MLPRowParallelOp(layer)
     if "o_proj" in prefix and oproj_tp_enable():
         return OProjRowParallelOp(layer)
@@ -681,3 +684,19 @@ def get_replicated_op(disable_tp, prefix,
         return None
 
     return CustomReplicatedOp(layer)
+
+
+def maybe_is_moe_layer(prefix: str) -> bool:
+    from vllm.config import get_current_vllm_config
+    match = re.search(r'layers\.(\d+)\.', prefix)
+    layer_idx = int(match.group(1))
+    vllm_config = get_current_vllm_config()
+    config = vllm_config.model_config.hf_config
+    n_routed_experts = getattr(config, 'n_routed_experts', 0)
+    first_k_dense_replace = getattr(config, 'first_k_dense_replace',
+                                    float('inf'))
+    moe_layer_freq = getattr(config, 'moe_layer_freq', 1)
+    is_moe_layer = (n_routed_experts is not None
+                    and layer_idx >= first_k_dense_replace
+                    and layer_idx % moe_layer_freq == 0)
+    return is_moe_layer
