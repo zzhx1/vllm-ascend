a543452ae224e1141857bd1152754868b93f3523
chenxiao <Jaychou1620@Gmail.com>
2026-01-08 20:34:06 +0800

    utfix:add get vllm config patch
    Signed-off-by: chenxiao <Jaychou1620@Gmail.com>

--- tests/ut/attention/test_sfa_v1.py
+++ tests/ut/attention/test_sfa_v1.py
@@ -33,7 +33,7 @@ class TestAscendSFABackend(TestBase):
 
 
 class TestAscendSFAMetadata(TestBase):
-
+    @patch("vllm_ascend.attention.sfa_v1.get_current_vllm_config")
     def test_ascend_sfa_metadata_default(self):
         has_prefill = True
         num_actual_tokens = 100
@@ -82,7 +82,7 @@ class TestAscendSFAMetadata(TestBase):
 
 
 class TestAscendSFAMetadataBuilder(TestBase):
-
+    @patch("vllm_ascend.attention.sfa_v1.get_current_vllm_config")
     def test_ascend_sfa_metadata_builder_default(self):
         kv_cache_spec = MagicMock()
         layer_names = ["layer1", "layer2"]
