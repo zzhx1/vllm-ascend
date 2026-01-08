8e417f2af0aeaccbdcc02f4900b5409d36094d68
Kurumi5210 <jaychou1620@gmail.com>
2026-01-08 23:30:31 +0800

    bugfix: give enable dsa cp UT-sfae
    Signed-off-by: Kurumi5210 <jaychou1620@gmail.com>

--- vllm_ascend/utils.py
+++ vllm_ascend/utils.py
@@ -1165,9 +1165,20 @@ def singleton(cls):
 @lru_cache(maxsize=1)
 def enable_dsa_cp() -> bool:
     from vllm.config import get_current_vllm_config
+
     vllm_config = get_current_vllm_config()
-    is_ds_v32 = hasattr(vllm_config.model_config.hf_text_config, "index_topk")
-    return is_ds_v32 and enable_sp()
+    if vllm_config is None:
+        return False
+
+    model_config = getattr(vllm_config, "model_config", None)
+    if model_config is None:
+        return False
+
+    hf_text_config = getattr(model_config, "hf_text_config", None)
+    if hf_text_config is None:
+        return False
+
+    return hasattr(hf_text_config, "index_topk")
 
 
 @lru_cache(maxsize=1)
