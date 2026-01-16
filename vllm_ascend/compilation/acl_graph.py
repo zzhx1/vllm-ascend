bec29f5130afd348c16b100c1ad723c733f5229a
Angazenn <92204292+Angazenn@users.noreply.github.com>
2026-01-16 23:29:21 +0800

    Revert "[v0.13.0][bugfix]Fix graph sync (#5809)" (#5966)
    ### What this PR does / why we need it?
    reverts #5809 to fix accuracy issues when using piecewise graph
    
    Signed-off-by: Angazenn <supperccell@163.com>

--- vllm_ascend/compilation/acl_graph.py
+++ vllm_ascend/compilation/acl_graph.py
@@ -192,13 +192,12 @@ class ACLGraphWrapper:
                 f"got {new_input_addresses}")
 
         logger.info_once("Replaying aclgraph")
-        # In async scheduling or multi-threaded (MT) scenarios when graph mode is FULL, it is possible that
+        # In async scheduling or multi-threaded (MT) scenarios, it is possible that
         # the CPU's record event (from update_attn_params) for the iteration i completes
         # before the grph replay of iteration i-1.
         # To ensure proper ordering, we must call synchronize here before replaying,
         # so that update_attn_params only executes after the previous graph replay has fully completed.
-        if self.runtime_mode == CUDAGraphMode.FULL:
-            torch.npu.synchronize()
+        torch.npu.synchronize()
         entry.aclgraph.replay()
         return entry.output
 
