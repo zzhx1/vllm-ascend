c80a50ca0d2fcee890a1dbb9c93886b57a0ba7dc
shaopeng-666 <lishaopeng21@huawei.com>
2026-01-21 11:57:50 +0800

    Revert "[0.13.0][cherry-pick][bugfix] fix bug of triton mrope" (#6075)
    Reverts vllm-project/vllm-ascend#6009
    
    Signed-off-by: 李少鹏 <lishaopeng21@huawei.com>

--- vllm_ascend/ops/rotary_embedding.py
+++ vllm_ascend/ops/rotary_embedding.py
@@ -25,10 +25,7 @@ from vllm.model_executor.layers.rotary_embedding import (
     DeepseekScalingRotaryEmbedding, MRotaryEmbedding, RotaryEmbedding,
     YaRNScalingRotaryEmbedding)
 from vllm.model_executor.layers.rotary_embedding.common import ApplyRotaryEmb
-from vllm.triton_utils import HAS_TRITON
-
-if HAS_TRITON:
-    from vllm.model_executor.layers.rotary_embedding.mrope import triton_mrope
+from vllm.model_executor.layers.rotary_embedding.mrope import triton_mrope
 
 from vllm_ascend.platform import NPUPlatform
 from vllm_ascend.utils import (AscendDeviceType, enable_custom_op,
@@ -529,38 +526,10 @@ class AscendDeepseekScalingRotaryEmbedding(DeepseekScalingRotaryEmbedding):
         return q_pe, k_pe
 
 
+_QWEN3_VL_MROPE_SECTION = [24, 20, 20]
+
+
 class AscendMRotaryEmbedding(MRotaryEmbedding):
-
-    def forward_triton(self,
-                       positions: torch.Tensor,
-                       query: torch.Tensor,
-                       key: torch.Tensor | None = None,
-                       offsets: torch.Tensor | None = None):
-        assert positions.ndim == 2
-        assert key is not None
-
-        self._match_cos_sin_cache_dtype(query)
-        cos_sin = self.cos_sin_cache[positions]  # type: ignore
-        cos, sin = cos_sin.chunk(2, dim=-1)
-        self.cos = cos.contiguous()
-        self.sin = sin.contiguous()
-        query_shape = query.shape
-        key_shape = key.shape
-
-        assert self.mrope_section
-
-        q, k = triton_mrope(
-            query,
-            key,
-            self.cos,
-            self.sin,
-            self.mrope_section,
-            self.head_size,
-            self.rotary_dim,
-            self.mrope_interleaved,
-        )
-
-        return q.reshape(query_shape), k.reshape(key_shape)
 
     def forward_oot(
         self,
@@ -568,8 +537,8 @@ class AscendMRotaryEmbedding(MRotaryEmbedding):
         query: torch.Tensor,
         key: torch.Tensor,
     ):
-        if HAS_TRITON and positions.ndim == 2:
-            # todo: need cann update in 8.5.0
+        # use triton mrope for Qwen3-VL
+        if self.mrope_section == _QWEN3_VL_MROPE_SECTION:
             return self.forward_triton(positions, query, key)
 
         if self.mrope_section != [16, 24, 24] or \
@@ -597,6 +566,35 @@ class AscendMRotaryEmbedding(MRotaryEmbedding):
                                          rotary_mode='half')
 
         return query, key
+
+    def forward_triton(
+        self,
+        positions: torch.Tensor,
+        query: torch.Tensor,
+        key: torch.Tensor | None = None,
+        offsets: torch.Tensor | None = None,
+    ) -> tuple[torch.Tensor, torch.Tensor | None]:
+        assert positions.ndim == 2
+        assert key is not None
+
+        self._match_cos_sin_cache_dtype(query)
+        cos_sin = self.cos_sin_cache[positions]
+        cos, sin = cos_sin.chunk(2, dim=-1)
+        query_shape = query.shape
+        key_shape = key.shape
+
+        assert self.mrope_section
+        q, k = triton_mrope(
+            query,
+            key,
+            cos,
+            sin,
+            self.mrope_section,
+            self.head_size,
+            self.rotary_dim,
+            self.mrope_interleaved,
+        )
+        return q.reshape(query_shape), k.reshape(key_shape)
 
 
 class AscendApplyRotaryEmb(ApplyRotaryEmb):
