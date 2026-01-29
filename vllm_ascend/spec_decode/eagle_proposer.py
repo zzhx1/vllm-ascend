6a7b3bc29cdd4c0bc25c87396b7567bb46c58607
Sergey-Zlobin <52936797+Sergey-Zlobin@users.noreply.github.com>
2026-01-29 11:44:30 +0300

    Qwen3-VL-MoE EAGLE support for vLLM-Ascend (#6327)
    ### What this PR does / why we need it?
    Qwen3-VL-MoE EAGLE support for vLLM-Ascend
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    The patch tested with Qwen3-VL-30B-A3B-Instruct model
    
    - vLLM version: v0.14.1
    - vLLM main:
    https://github.com/vllm-project/vllm/commit/dc917cceb877dfd13f98c538c4c96158047d98bd
    
    Signed-off-by: Sergey_Zlobin <sirg_zlobin@mail.ru>

--- vllm_ascend/spec_decode/eagle_proposer.py
+++ vllm_ascend/spec_decode/eagle_proposer.py
@@ -198,6 +198,7 @@ class EagleProposer(VllmEagleProposer):
             if self.get_model_name(model) in [
                     "Qwen2_5_VLForConditionalGeneration",
                     "Qwen3VLForConditionalGeneration",
+                    "Qwen3VLMoeForConditionalGeneration",
             ]:
                 self.model.config.image_token_index = model.config.image_token_id
             elif self.get_model_name(
