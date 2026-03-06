a2696006d1756ba827dbf0bcf3386ecc1d3b01e9
Zetong Li <48438720+slippersss@users.noreply.github.com>
2026-03-06 17:11:22 +0800

    [Refactor][EAGLE] 8/N delete mtp_proposer (re-pull) (#7033)
    ### What this PR does / why we need it?
    **NOTE: This PR is re-pull of #7016 since ci mistakenly marked
    unfinished pr as having passed.**
    
    This PR aims to delete mtp_proposer. By fixing a bug in both dsv32 and
    glm5, now it should be ok to remove mtp_proposer. The bug is actually
    about unnecessary slicing of `slot_mapping`.
    
    ### Does this PR introduce _any_ user-facing change?
    N/A
    
    ### How was this patch tested?
    by ci
    
    - vLLM version: v0.16.0
    - vLLM main:
    https://github.com/vllm-project/vllm/commit/4034c3d32e30d01639459edd3ab486f56993876d
    
    ---------
    
    Signed-off-by: Zetong Li <slippersss@126.com>

--- vllm_ascend/spec_decode/__init__.py
+++ vllm_ascend/spec_decode/__init__.py
@@ -18,7 +18,6 @@
 #
 from vllm_ascend.spec_decode.eagle_proposer import AscendEagleProposer
 from vllm_ascend.spec_decode.medusa_proposer import AscendMedusaProposer
-from vllm_ascend.spec_decode.mtp_proposer import AscendMtpProposer
 from vllm_ascend.spec_decode.ngram_proposer import AscendNgramProposer
 from vllm_ascend.spec_decode.suffix_proposer import AscendSuffixDecodingProposer
 
@@ -30,9 +29,7 @@ def get_spec_decode_method(method, vllm_config, device, runner):
         return AscendSuffixDecodingProposer(vllm_config, runner)
     elif method == "medusa":
         return AscendMedusaProposer(vllm_config, device)
-    elif method in ("eagle", "eagle3"):
+    elif method in ("eagle", "eagle3", "mtp"):
         return AscendEagleProposer(vllm_config, device, runner)
-    elif method == "mtp":
-        return AscendMtpProposer(vllm_config, device, runner)
     else:
         raise ValueError(f"Unknown speculative decoding method: {method}")
