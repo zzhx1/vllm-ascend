[BugFix] Fixed eagle's precision problem when num_spec > 1 for model_runner_v2 (#8033)
### What this PR does / why we need it?

There's a precision bug in v2's eagle when num_spec > 1.
Below are the modification points.

- Fixed `torch.gather`'s usage bug (`pos` and
`self.input_buffers.positions[:num_reqs]` **must** share same address,
or it will cause precision problem).
- Updated `seq_lens_list` correctly.

Please view model runner v2's plan
https://github.com/vllm-project/vllm-ascend/issues/5208 for detail.

### Does this PR introduce _any_ user-facing change?
N/A.

### How was this patch tested?

Test code is shown as below:

```python
os.environ["VLLM_USE_V2_MODEL_RUNNER"] = "1"

if __name__ == '__main__':
    prompts = [
        "Who are you?",
    ]

    sampling_params = SamplingParams(temperature=0.0, top_p=0.95, top_k=40, max_tokens=20)
    llm = LLM(
        model="/home/model/Meta-Llama-3.1-8B-Instruct",
        tensor_parallel_size=1,
        enforce_eager=True,
        distributed_executor_backend="mp",
        gpu_memory_utilization=0.7,
        async_scheduling=True,
        disable_log_stats=False,
        speculative_config={
            "model": "/home/model/EAGLE3-LLaMA3.1-Instruct-8B",
            "disable_padded_drafter_batch": False,
            "method": "eagle3",
            "num_speculative_tokens": 2,
        },

        compilation_config={
            "cudagraph_mode": "FULL_DECODE_ONLY",
            "cudagraph_capture_sizes": [4],
        },

        max_model_len=4096, 
        enable_prefix_caching=False
    )

    outputs = llm.generate(prompts, sampling_params)
    for output in outputs:
        prompt = output.prompt
        generated_text = output.outputs[0].text
        print(output.outputs[0].token_ids)
        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")

    metrics = llm.get_metrics()

    total_num_output_tokens = sum(
        len(output.outputs[0].token_ids) for output in outputs
    )
    num_drafts = 0

vllm_ascend/patch/__init__.py
@@ -344,17 +344,15 @@
 #    Future Plan:
 #       Remove this patch when vLLM aligns with the latest processor implementation.
 #
-# ** 10. File: worker/patch_v2/patch_eagle.py**
+# ** 10. File: worker/patch_qwen3vl.py**
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#   1. `vllm.v1.worker.gpu.spec_decode.eagle.EagleSpeculator.propose`
+#   1. `vllm.model_executor.models.qwen3_vl.Qwen3VLForConditionalGeneration._get_deepstack_input_embeds`
 #    Why:
-#       `propose` method use torch.gather, but the gather operator will
-#       pollute the arguments passed to it. the bug is reported to huawei
-#       CANN team, but not fixed yet.
+#       support flash comm v1 for qwen3vl.
 #    How：
-#       clone the out attribute ahead of gather to avoid the bug.
+#       override _get_deepstack_input_embeds method with the flash comm v1 implementation.
 #    Future Plan:
-#       Remove this patch when cann fix the gather bug.
+#       Remove this patch when https://github.com/vllm-project/vllm-ascend/issues/5712 is completed.
 #
 # ** 11. File: worker/patch_unquantized_gemm.py**
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -689,12 +687,3 @@
 #       when using mrope.
 #    Future Plan:
 #       Remove this patch when vllm-ascend supports pattern matching for this fused kernel.
-# ** 29. File: worker/patch_qwen3vl.py**
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#   1. `vllm.model_executor.models.qwen3_vl.Qwen3VLForConditionalGeneration._get_deepstack_input_embeds`
-#    Why:
-#       support flash comm v1 for qwen3vl.
-#    How：
-#       override _get_deepstack_input_embeds method with the flash comm v1 implementation.
-#    Future Plan:
-#       Remove this patch when https://github.com/vllm-project/vllm-ascend/issues/5712 is completed.

vllm_ascend/worker/v2/spec_decode/eagle/speculator.py
@@ -75,7 +75,7 @@ class AscendEagleSpeculator(EagleSpeculator):
         self.input_batch = input_batch
         # wrap build_attn_metadata to use Ascend attention metadata building.
         # so we can call super().propose() directly.
-        with build_attn_metadata_wrapper():
+        with build_attn_metadata_wrapper(), torch_gather_wrapper():
             return super().propose(
                 input_batch,
                 attn_metadata,
@@ -159,11 +159,13 @@ class AscendEagleSpeculator(EagleSpeculator):
 
         attn_state = AscendAttentionState.DecodeOnly
         seq_lens_cpu = self._get_seq_lens_cpu()
+        seq_lens_list = seq_lens_cpu.tolist()
         # attn_metadata is build in vllm's super class.
         # We need to update attn_state for each layer's metadata.
         for metadata in attn_metadata.values():
             metadata.attn_state = attn_state
             metadata.seq_lens_cpu = seq_lens_cpu
+            metadata.seq_lens_list = seq_lens_list
 
     def _get_seq_lens_cpu(self) -> torch.Tensor:
         """Get seq_lens_cpu from input_batch."""
@@ -181,3 +183,28 @@ def build_attn_metadata_wrapper():
         yield
     finally:
         vllm.v1.worker.gpu.spec_decode.eagle.speculator.build_attn_metadata = original_func
+
+
+# TODO Remove this patch when cann fix the gather bug.
+# NOTE(Ronald1995): torch.gather will pollute the cache such as self.input_buffers.positions
+# the bug is reported to huawei CANN team, but not fixed yet.
+# NOTE(drslark): make a temporary patch only for `torch.gather`
+_original_gather = torch.gather
+
+
+def gather(input, dim, index, *, sparse_grad=False, out=None):
+    if out is None:
+        return _original_gather(input, dim, index, sparse_grad=sparse_grad)
+    out[:] = _original_gather(input, dim, index, sparse_grad=sparse_grad)
+    return out
+
+
+@contextmanager
+def torch_gather_wrapper():
+    """Context manager to override torch.gather for Ascend NPUs."""
+    original_gather = torch.gather
+    try:
+        torch.gather = gather
+        yield
+    finally:
+        torch.gather = original_gather