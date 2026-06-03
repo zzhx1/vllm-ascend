66ea656ea02492baf95cdea7eda0b588c9c1ad19
yupeng <507435917@qq.com>
2026-06-03 10:52:14 +0800

    [Test] Add a testcase of speculative_decoding + LoRA (#7428)
    ### What this PR does / why we need it?
    Add a testcase of speculative_decoding + LoRA.
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    pytest -sv
    tests/e2e/singlecard/spec_decode/test_lora_with_spec_decode.py
    
    - vLLM version: v0.20.2
    - vLLM main:
    https://github.com/vllm-project/vllm/commit/9090368b650896bf5fc990c921df7eb4c20355a5
    
    ---------
    
    Signed-off-by: paulyu12 <507435917@qq.com>
    Signed-off-by: yupeng <507435917@qq.com>

---
+++ tests/e2e/singlecard/spec_decode/test_lora_with_spec_decode.py
@@ -0,0 +1,116 @@
+"""
+This script contains:
+1. test lora with speculative decoding for batch inference
+"""
+
+import random
+
+import numpy as np
+import pytest
+import torch
+from vllm import LLM, SamplingParams
+from vllm.lora.request import LoRARequest
+
+LORA_TEST_PROMPT_MAP: dict[str, str] = {}
+
+LORA_TEST_PROMPT_MAP["vllm-ascend/qwen-linear-algebra-coder"] = """
+### INSTRUCTION:
+You are an AI assistant that generates Python code to solve linear
+algebra problems.
+
+### PROBLEM:
+Find the eigenvalues and eigenvectors of the following 3x3 matrix:
+[[3, 2, 0],
+ [2, 3, 0],
+ [0, 0, 2]]
+
+### OUTPUT FORMAT (STRICT):
+Numbers should be represented as integers only.
+
+### PYTHON SOLUTION:
+"""
+
+SEED = 42
+
+
+@pytest.mark.parametrize(
+    "model_setup",
+    [
+        (
+            "eagle3",
+            "Qwen/Qwen3-1.7B",
+            "vllm-ascend/Qwen3-1.7B_eagle3",
+            "vllm-ascend/qwen-linear-algebra-coder",
+            1,
+        )
+    ],
+)
+def test_batch_inference_correctness(
+    model_setup: tuple[str, str, str, str, int],
+):
+    """
+    Compare the outputs of a LLM with only Lora and a LLM with both SD and Lora.
+    Should be the same and no failure when doing batch inference.
+    model_setup: (method, model_name, spec_model_name, lora_path, tp_size)
+    """
+    # Disable randomness
+    torch.manual_seed(SEED)
+    np.random.seed(SEED)
+    random.seed(SEED)
+    torch.use_deterministic_algorithms(True)
+
+    method, model_name, spec_model_name, lora_path, tp_size = model_setup
+    prompts = [LORA_TEST_PROMPT_MAP[lora_path]] * 100
+    lora_request = LoRARequest("adapter", 1, lora_path)
+    sampling_params = SamplingParams(temperature=0.0, top_p=1.0, top_k=-1, seed=SEED, max_tokens=128)
+
+    # without speculative decoding
+    ref_llm = LLM(
+        model=model_name,
+        trust_remote_code=True,
+        tensor_parallel_size=tp_size,
+        max_model_len=2048,
+        max_num_seqs=4,
+        enable_lora=True,
+        max_loras=1,
+        max_cpu_loras=1,
+        max_lora_rank=16,
+    )
+    ref_outputs = ref_llm.generate(prompts, sampling_params, lora_request=lora_request)
+    del ref_llm
+
+    # speculative decoding
+    lora_spec_llm = LLM(
+        model=model_name,
+        trust_remote_code=True,
+        tensor_parallel_size=tp_size,
+        speculative_config={
+            "method": method,
+            "model": spec_model_name,
+            "num_speculative_tokens": 3,
+            "max_model_len": 2048,
+        },
+        max_model_len=2048,
+        max_num_seqs=4,
+        enable_lora=True,
+        max_loras=1,
+        max_cpu_loras=1,
+        max_lora_rank=16,
+    )
+    lora_spec_outputs = lora_spec_llm.generate(prompts, sampling_params, lora_request=lora_request)
+    del lora_spec_llm
+
+    matches = 0
+    misses = 0
+    for ref_output, spec_output in zip(ref_outputs, lora_spec_outputs):
+        if ref_output.outputs[0].text == spec_output.outputs[0].text:
+            matches += 1
+        else:
+            misses += 1
+            print(f"ref_output: {ref_output.outputs[0].text}")
+            print(f"spec_output: {spec_output.outputs[0].text}")
+
+    # Heuristic: expect at least 90% of the prompts to match exactly
+    # Upon failure, inspect the outputs to check for inaccuracy.
+    print(f"match ratio: {matches}/{len(ref_outputs)}")
+    assert matches > int(0.90 * len(ref_outputs))
